ce117d4fb08ee2c89feb5d02c4736873df8c3702
Jacob Dick <jdick@solutionreach.com>
2019-09-23 05:16:50 -0600

    add created_at and updated_at fields to all models

--- common/models/base.py
+++ common/models/base.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class BaseModel(models.Model):
     """The base model from which all other models inherit."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
+    id          = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at  = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at  = models.DateTimeField(auto_now=True, editable=False)
     class Meta:
         abstract = True
